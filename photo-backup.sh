72590ff4eae7efb23e941ce42bd01a878fcf8537
Thomas Vincent <vrince@gmail.com>
2018-01-14 23:33:26 -0500

    variable clean-up, use config file for google client

--- photo-backup.sh
+++ photo-backup.sh
@@ -18,25 +18,32 @@ unlock() { lock u;} # drop a lock
 
 exlockNow || echoLockedAndExit
 
-###
-ceol=`tput el` # terminfo clr_eol
+#config and credentials
+config_file=client.cfg
+credential_file=credential.cfg
 
-source config.sh
+if [ ! -f $config_file ]; then
+   echo "$config_file does not exist, please setup google project dance (see readme.md)"
+   exit 1
+fi
+. $config_file
 
+#temporary index folder
+index_dir="./index"
+mkdir -p $index_dir
+rm $index_dir/*
+
+#TODO add config for path
 nas_import_dir="/mnt/nas/Uploads"
 import_dir=""
 photo_root="/mnt/nas/Data/Photos"
 video_root="/mnt/nas/Data/Videos"
 photo_resized_root="/mnt/nas/Data/Photos_Resized"
+
 image_ext_regex="\.JPG$|\.JPEG$|\.jpg$|\.jpeg$"
 video_ext_regex="\.MOV$|\.mov$|\.AVI$|\.avi$|\.3GP$|\.3gp$|\.mp4$|\.MP4$"
-images_to_import_file=".images_to_import.txt"
-videos_to_import_file=".videos_to_import.txt"
-import_arguments=".import_arguments.txt"
-images_to_resize_file=".images_to_resize.txt"
-resize_arguments=".resize_arguments.txt"
+
 target_image_size=2048
-uploaded_files=".uploaded_files.txt"
 
 ###
 
@@ -58,10 +65,6 @@ do
         -v|--verbose)
         verbose_=yes
         echo "verbose($verbose_)"
-        ;;
-        -p|--progress)
-        progress_=yes
-        echo "progress($progress_)"
         ;;
         *)
         echo "unknown option($1)"
@@ -122,9 +125,9 @@ function importFile() {
 export -f importFile
 
 function googleAuth() {
-    if [ -s $my_creds ]; then
+    if [ -s $credential_file ]; then
         # if we already have a token stored, use it
-        source $my_creds
+        source $credential_file
         time_now=`date +%s`
     else
         scope="https://picasaweb.google.com/data/"
@@ -159,7 +162,7 @@ function googleAuth() {
                     awk -F' ' '{ print $3 }' | awk -F',' '{ print $1}')
         time_now=`date +%s`
         expires_at=$((time_now + expires_in - 60))
-        echo -e "access_token=$access_token\nrefresh_token=$refresh_token\nexpires_at=$expires_at" > $my_creds
+        echo -e "access_token=$access_token\nrefresh_token=$refresh_token\nexpires_at=$expires_at" > $credential_file
     fi
 
     # if our access token is expired, use the refresh token to get a new one
@@ -179,7 +182,7 @@ function googleAuth() {
                     awk -F' ' '{ print $3 }' | awk -F',' '{ print $1 }')
         time_now=`date +%s`
         expires_at=$(($time_now + $expires_in - 60))
-        echo -e "access_token=$access_token\nrefresh_token=$refresh_token\nexpires_at=$expires_at" > $my_creds
+        echo -e "access_token=$access_token\nrefresh_token=$refresh_token\nexpires_at=$expires_at" > $credential_file
     fi
 }
 export -f googleAuth
@@ -201,69 +204,58 @@ function upload() {
 }
 export -f upload
 
-function progress() {
-    if [ -z "$progress_" ]; then return ; fi
-    let _progress=($1*100/$2*100)/100
-    let _done=($_progress*4)/10
-    let _left=40-$_done
-    _fill=$(printf "%${_done}s")
-    _empty=$(printf "%${_left}s")
-    shift 2
-    printf "\r[${_fill// /#}${_empty// /-}] ${_progress}%% $@${ceol}"
-}
-
 ## Uploads
 function uploadAll () {
-    available_images_file=".available_images.txt"
-    uploaded_images_file=".uploaded_images.txt"
-    to_upload_photos_file=".to_upload_images.txt"
-
-    rm -f $to_upload_photos_file
+    available_images="$index_dir/available_images"
+    uploaded_images="$index_dir/uploaded_images"
+    upload_arguments="$index_dir/to_upload_images"
 
     #all files
     echo "extracting all available images ..."
-    find $photo_resized_root -type f | grep -E "$image_ext_regex" > $available_images_file
-    echo "images available : $(wc -l < $available_images_file)"
+    find $photo_resized_root -type f | grep -E "$image_ext_regex" > $available_images
+    echo "images available : $(wc -l < $available_images)"
 
     echo "extracting all uploaded images ..."
-    find $photo_resized_root -type f | grep -E "\.xml$" | rev | cut -f 2- -d '.' | rev > $uploaded_images_file
-    echo "images uploaded : $(wc -l < $uploaded_images_file)"
+    find $photo_resized_root -type f | grep -E "\.xml$" | rev | cut -f 2- -d '.' | rev > $uploaded_images
+    echo "images uploaded : $(wc -l < $uploaded_images)"
 
     echo "extracting difference ..."
-    grep -Fxvf $uploaded_images_file $available_images_file | sort -rn > $to_upload_photos_file
-    number_of_images_to_upload=$(wc -l < $to_upload_photos_file)
-    echo "images to upload : $number_of_images_to_upload"
+    grep -Fxvf $uploaded_images $available_images | sort -rn > $upload_arguments
 
-    if [ -f "$to_upload_photos_file" ] ; then 
+    echo "images to upload : $(wc -l < $upload_arguments)"
+
+    if [ -f "$upload_arguments" ] ; then 
         echo "uploading ..."
-        cat $to_upload_photos_file | parallel --bar --eta upload
+        cat $upload_arguments | parallel --bar --eta upload
     fi
 }
 
 function importAll() {
     #find all files
 
+    images_to_import="$index_dir/images_to_import"
+    videos_to_import="$index_dir/videos_to_import"
+
     if [ -n "$import_dir" ] ; then 
-        find $import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$image_ext_regex" > $images_to_import_file
-        find $import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$video_ext_regex" > $videos_to_import_file
+        find $import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$image_ext_regex" > $images_to_import
+        find $import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$video_ext_regex" > $videos_to_import
     fi
 
-    find $nas_import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$image_ext_regex" >> $images_to_import_file
-    find $nas_import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$video_ext_regex" >> $videos_to_import_file
+    find $nas_import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$image_ext_regex" >> $images_to_import
+    find $nas_import_dir -type f | grep -v '@Recycle' | grep -v '.Trash' | grep -E "$video_ext_regex" >> $videos_to_import
 
-    number_of_image_to_import=$(wc -l < $images_to_import_file)
-    number_of_video_to_import=$(wc -l < $videos_to_import_file)
+    echo "images to import : $(wc -l < $images_to_import)"
+    echo "videos to import : $(wc -l < $videos_to_import)"
 
-    echo "images to import : $number_of_image_to_import"
-    echo "videos to import : $number_of_video_to_import"
+    import_arguments="$index_dir/import_arguments"
 
     while IFS='' read -r image_file || [[ -n "$image_file" ]]; do
         echo "$image_file:$photo_root" >> $import_arguments
-    done < "$images_to_import_file"
+    done < "$images_to_import"
 
     while IFS='' read -r video_file || [[ -n "$video_file" ]]; do
         echo "$video_file:$video_root" >> $import_arguments
-    done < "$videos_to_import_file"
+    done < "$videos_to_import"
 
     if [ -f "$import_arguments" ] ; then  
         echo "importing ..."
@@ -274,11 +266,17 @@ function importAll() {
 function resizeAll() {
 
     echo "extracting images list to resize ..."
-    find $photo_root -type f -printf '%P\n' | grep -E "$image_ext_regex"  | grep -v "(2)." > all_images.list
-    find $photo_resized_root -type f -printf '%P\n' | grep -E "$image_ext_regex" > all_resized_images.list
-    grep -Fxvf all_resized_images.list all_images.list | sort -rn | awk -v p_root="$photo_root/" '{print p_root $0}' > $images_to_resize_file
-    number_of_image_to_resize=$(wc -l < $images_to_resize_file)
-    echo "images to resize : $number_of_image_to_resize"
+
+    images_index="$index_dir/all_images"
+    resized_images_index="$index_dir/all_resized_images"
+    images_to_resize="$index_dir/images_to_resize"
+    find $photo_root -type f -printf '%P\n' | grep -E "$image_ext_regex"  | grep -v "(2)." > $images_index
+    find $photo_resized_root -type f -printf '%P\n' | grep -E "$image_ext_regex" > $resized_images_index
+    grep -Fxvf $resized_images_index $images_index | sort -rn | awk -v p_root="$photo_root/" '{print p_root $0}' > $images_to_resize
+
+    echo "images to resize : $(wc -l < $images_to_resize)"
+
+    resize_arguments="$index_dir/resize_arguments"
 
     while IFS='' read -r image_file || [[ -n "$image_file" ]]; do
         source_dir=$(dirname "$image_file")
@@ -287,20 +285,13 @@ function resizeAll() {
         destination_dir="$photo_resized_root$relative_dir"
         mkdir -p "$destination_dir"
         echo "$image_file:$target_image_size:$destination_dir" >> $resize_arguments
-    done < "$images_to_resize_file"
+    done < "$images_to_resize"
     
     if [ -f "$resize_arguments" ] ; then  
         echo "resizing ..."
         cat $resize_arguments | parallel --bar --eta --colsep ':' resize {1} {2} {3}
     fi
 }
-
-#clean stuff
-rm -f $images_to_import_file
-rm -f $videos_to_import_file
-rm -f $import_arguments
-rm -f $images_to_resize_file
-rm -f $resize_arguments
 
 googleAuth
 importAll
